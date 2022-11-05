72deda84b328f1ce28f0154bf32ca242775f367e
nikuscs <nikuscs@users.noreply.github.com>
2022-11-05 00:10:52 +0000

    Fix styling

--- src/Datatables/Actions/Concerns/CanBeExecuted.php
+++ src/Datatables/Actions/Concerns/CanBeExecuted.php
@@ -8,12 +8,14 @@ use Illuminate\Support\Collection;
 trait CanBeExecuted
 {
     protected null | Closure | string $executeUsing = null;
+
     protected ?string $executeUsingMethod = null;
 
     public function executeUsing(Closure|null|string $closureOrInvokable = null, ?string $method = null): static
     {
         $this->executeUsing = $closureOrInvokable;
         $this->executeUsingMethod = $method;
+
         return $this;
     }
 
@@ -24,29 +26,25 @@ trait CanBeExecuted
 
     public function execute(?Collection $ids = null): void
     {
-
         // Execute the before Hook
-        if(class_implements($this,HasHooks::class) && $this->onBefore !== null){
+        if (class_implements($this, HasHooks::class) && $this->onBefore !== null) {
             app()->call($this->onBefore, [
                 'action' => $this,
                 'ids' => $ids,
             ], $this->executeUsingMethod);
         }
 
-        try{
-
+        try {
             // Do nothing
-            if($this->executeUsing === null){
-
-            }
-            elseif(method_exists($this,'handle')){
-                app()->call([$this,'handle'], [
+            if ($this->executeUsing === null) {
+            } elseif (method_exists($this, 'handle')) {
+                app()->call([$this, 'handle'], [
                     'action' => $this,
                     'ids' => $ids,
                 ], $this->executeUsingMethod);
             }
             // Action should be executed as a closure
-            elseif($this->getExecuteUsing() instanceof Closure) {
+            elseif ($this->getExecuteUsing() instanceof Closure) {
                 app()->call($this->getExecuteUsing(), [
                     'action' => $this,
                     'ids' => $ids,
@@ -54,35 +52,32 @@ trait CanBeExecuted
             }
 
             // Action is a class, lets invoke
-            elseif(is_string($this->getExecuteUsing())) {
+            elseif (is_string($this->getExecuteUsing())) {
                 app()->call($this->getExecuteUsing(), [
-                    'action' => $this,
-                    'ids' => $ids,
-                ],$this->executeUsingMethod);
-            }
-
-            // Execute the After Hook
-            if(class_implements($this,HasHooks::class) && $this->onAfter !== null){
-                app()->call($this->onAfter, [
                     'action' => $this,
                     'ids' => $ids,
                 ], $this->executeUsingMethod);
             }
 
-        }catch (\Exception $e){
-
+            // Execute the After Hook
+            if (class_implements($this, HasHooks::class) && $this->onAfter !== null) {
+                app()->call($this->onAfter, [
+                    'action' => $this,
+                    'ids' => $ids,
+                ], $this->executeUsingMethod);
+            }
+        } catch (\Exception $e) {
             // Execute on Exception/Failed
-            if(class_implements($this,HasHooks::class) && $this->onFailed !== null){
+            if (class_implements($this, HasHooks::class) && $this->onFailed !== null) {
                 app()->call($this->onFailed, [
                     'action' => $this,
                     'ids' => $ids,
                     'exception' => $e,
                 ], $this->executeUsingMethod);
             }
-
         } finally {
             // Execute the before Hook
-            if(class_implements($this,HasHooks::class) && $this->onFinished !== null){
+            if (class_implements($this, HasHooks::class) && $this->onFinished !== null) {
                 app()->call($this->onFinished, [
                     'action' => $this,
                     'ids' => $ids,
--- src/Datatables/Table/Concerns/HasActions.php
+++ src/Datatables/Table/Concerns/HasActions.php
@@ -22,16 +22,15 @@ trait HasActions
     protected function actionsToArray(): array
     {
         return collect($this->actions)->map(function ($action) {
-
-            if(is_string($action)) {
+            if (is_string($action)) {
                 return app()->make($action)->toArray();
             }
 
-            if($action instanceof Action) {
+            if ($action instanceof Action) {
                 return $action->toArray();
             }
 
-            if(is_array($action)){
+            if (is_array($action)) {
                 return $action;
             }
 
