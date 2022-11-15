6cc294bc66ae95ed86a7b3541987981e495d663d
nikuscs <nikuscs@users.noreply.github.com>
2022-11-15 19:18:22 +0000

    Fix styling

--- src/Datatables/PendingAction/PendingAction.php
+++ src/Datatables/PendingAction/PendingAction.php
@@ -3,7 +3,6 @@
 namespace Flavorly\VanillaComponents\Datatables\PendingAction;
 
 use Flavorly\VanillaComponents\Core\Concerns as CoreConcerns;
-use Flavorly\VanillaComponents\Datatables\PendingAction\Concerns;
 
 class PendingAction
 {
