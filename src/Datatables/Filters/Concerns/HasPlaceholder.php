4fd59f4689049d2d0db7f7010cd5341569102899
nikuscs <nikuscs@gmail.com>
2022-11-19 22:50:20 +0000

    Bugfix filters

---
+++ src/Datatables/Filters/Concerns/HasPlaceholder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Flavorly\VanillaComponents\Datatables\Filters\Concerns;
+
+use Closure;
+
+trait HasPlaceholder
+{
+    protected string | Closure | null $placeholder = null;
+
+    public function placeholder(string | Closure | null $placeholder): static
+    {
+        $this->placeholder = $placeholder;
+
+        return $this;
+    }
+
+    public function getPlaceholder(): string | null
+    {
+        return $this->evaluate($this->placeholder);
+    }
+}
--- src/Datatables/Filters/Filter.php
+++ src/Datatables/Filters/Filter.php
@@ -2,6 +2,7 @@
 
 namespace Flavorly\VanillaComponents\Datatables\Filters;
 
+use Flavorly\VanillaComponents\Core\Components\Concerns\HasPlaceholder;
 use Flavorly\VanillaComponents\Core\Concerns as CoreConcerns;
 use Flavorly\VanillaComponents\Core\Contracts as CoreContracts;
 use Flavorly\VanillaComponents\Datatables\Concerns as BaseConcerns;
@@ -20,6 +21,7 @@ class Filter implements CoreContracts\HasToArray
     use Concerns\HasValueAndDefaultValue;
     use Concerns\HasComponentTypes;
     use Concerns\InteractsWithTableQuery;
+    use Concerns\HasPlaceholder;
     use Macroable;
 
     public function toArray(): array
@@ -28,7 +30,7 @@ class Filter implements CoreContracts\HasToArray
             'name' => $this->getName(),
             'label' => $this->getLabel(),
             'component' => $this->getComponent(),
-            'placeholder' => Arr::get($this->getAttributes(), 'placeholder'),
+            'placeholder' => $this->getPlaceholder() ?? Arr::get($this->getAttributes(), 'placeholder'),
             'attributes' => $this->getAttributes(),
             'value' => $this->getValue(),
             'defaultValue' => $this->getDefaultValue(),
